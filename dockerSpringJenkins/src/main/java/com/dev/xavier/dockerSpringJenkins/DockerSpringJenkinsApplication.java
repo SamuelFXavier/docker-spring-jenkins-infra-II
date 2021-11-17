c668f9d24c848580294be0b90699a1616433ab55
Samuel Xavier <xavierfsamuel@gmail.com>
2021-11-17 16:38:06 -0300

    extração do arquivo no diretorio principal e clonagem com gitHub

---
+++ dockerSpringJenkins/src/main/java/com/dev/xavier/dockerSpringJenkins/DockerSpringJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.xavier.dockerSpringJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringJenkinsApplication.class, args);
+	}
+
+}
---
+++ dockerSpringJenkins/src/test/java/com/dev/xavier/dockerSpringJenkins/DockerSpringJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.xavier.dockerSpringJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
